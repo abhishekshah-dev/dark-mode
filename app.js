0fe8fd54a81c93ec050367f8bc88a557974453bb
Abhishek Shah <AbhishekMacMini@Abhisheks-Mac-mini.local>
2022-08-29 18:16:39 +0530

    dark-mode and momentjs project

---
+++ app.js
@@ -0,0 +1,28 @@
+const toggleBtn = document.querySelector(".btn");
+const articlesContainer = document.querySelector(".articles");
+
+toggleBtn.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark-theme");
+});
+
+const articlesData = articles
+  .map((article) => {
+    const { date, length, snippet, title } = article;
+
+    const formatDate = moment(date).format("MMMM Do, YYYY");
+    return `<article class="post">
+  <h2>${title}</h2>
+  <div class="post-info">
+    <span>${formatDate}</span>
+    <span>${length} min read</span>
+  </div>
+  <p>
+  ${snippet}
+  </p>
+</article>`;
+  })
+  .join("");
+
+articlesContainer.innerHTML = articlesData;
+
+console.log(moment);
